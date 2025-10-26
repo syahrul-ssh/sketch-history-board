3caec811928d28d4be83db6066daeef5778afed7
Syahrul Safarudin Hasan <syahrul.safarudinh9@gmail.com>
2025-10-27 01:33:54 +0700

    FE(feat): setup frontend needs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_BASE_URL:
+      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api",
+  },
 };
 
 export default nextConfig;
